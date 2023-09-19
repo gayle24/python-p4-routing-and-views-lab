14a7febd4896ad67a9b903f7e6fbafb9857ede72
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-09-19 22:27:14 +0300

    adding initial route

--- server/app.py
+++ server/app.py
@@ -4,5 +4,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def test_index_text():
+    return f'<h1>Python Operation with Flask Routing and Views</h1>'
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
